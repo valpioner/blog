9bbc40cec83623cacf00a20280ebf1791c4fbbeb
Valentyn Vasylevskyy <valentun9@gmail.com>
2020-09-09 01:04:58 +0300

    fixed build again, hopefully

--- scully.blog.config.ts
+++ scully.blog.config.ts
@@ -1,9 +1,14 @@
 import { ScullyConfig } from '@scullyio/scully';
+import { OptimizeCSSPlugin } from 'scully-plugin-optimize-css';
+import { MinifyHtml } from 'scully-plugin-minify-html';
+import { DisableAngular } from 'scully-plugin-disable-angular';
+
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'blog',
   // pluginDir: './scully/plugins/',
   outDir: './dist/static',
+  defaultPostRenderers: [MinifyHtml, OptimizeCSSPlugin, DisableAngular],
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
