0cccd3f760a065fdc84551da766f8f19d77c3c0e
Valentyn Vasylevskyy <valentun9@gmail.com>
2020-09-09 00:18:20 +0300

    Revert "fixed build"
    This reverts commit 3aa2c653b380d7ae8fc3ddb3f28491125672bb73.

--- scully.blog.config.ts
+++ scully.blog.config.ts
@@ -4,40 +4,40 @@ import { MinifyHtml } from 'scully-plugin-minify-html';
 import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
 import { DisableAngular } from 'scully-plugin-disable-angular';
 
-// const SitemapPlugin = getSitemapPlugin();
-// setPluginConfig(SitemapPlugin, {
-//   urlPrefix: 'https://gamma.stream',
-//   sitemapFilename: 'sitemap.xml',
-//   changeFreq: 'monthly',
-//   priority: [
-//     '1.0',
-//     '0.9',
-//     '0.8',
-//     '0.7',
-//     '0.6',
-//     '0.5',
-//     '0.4',
-//     '0.3',
-//     '0.2',
-//     '0.1',
-//     '0.0',
-//   ],
-//   ignoredRoutes: ['/404'],
-//   routes: {
-//     '/products/:productId': {
-//       changeFreq: 'daily',
-//       priority: '0.9',
-//       sitemapFilename: 'sitemap-products.xml',
-//     },
-//   },
-// });
+const SitemapPlugin = getSitemapPlugin();
+setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://gamma.stream',
+  sitemapFilename: 'sitemap.xml',
+  changeFreq: 'monthly',
+  priority: [
+    '1.0',
+    '0.9',
+    '0.8',
+    '0.7',
+    '0.6',
+    '0.5',
+    '0.4',
+    '0.3',
+    '0.2',
+    '0.1',
+    '0.0',
+  ],
+  ignoredRoutes: ['/404'],
+  routes: {
+    '/products/:productId': {
+      changeFreq: 'daily',
+      priority: '0.9',
+      sitemapFilename: 'sitemap-products.xml',
+    },
+  },
+});
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'blog',
   // pluginDir: './scully/plugins/',
   outDir: './dist/static',
-  defaultPostRenderers: [MinifyHtml, OptimizeCSSPlugin /*, DisableAngular*/],
+  defaultPostRenderers: [MinifyHtml, OptimizeCSSPlugin, DisableAngular],
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
